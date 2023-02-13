b989d05dec5ad7a96f89f5e298035c2d8894d073
Claas Hürkamp <claas.huerkamp@posteo.net>
2023-02-13 17:35:39 +0100

    Add Application class

---
+++ src/application/application.py
@@ -0,0 +1,21 @@
+"""
+pyXrm Application (c) 2023 Claas Huerkamp <claas.huerkamp@posteo.net>
+All Rights Reserved
+"""
+
+
+class Application:
+    """
+    Main controller
+    """
+
+    def run(self) -> None:
+        """
+        Run main controller for pyXrm
+
+        Parameters:
+        self
+
+        Return:
+        None
+        """
