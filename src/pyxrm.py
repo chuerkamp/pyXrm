da9538a75b8893bef5f9875b33eb085240ea786b
Claas Hürkamp <claas.huerkamp@posteo.net>
2023-02-13 17:29:04 +0100

    Add main entry point for application

---
+++ src/pyxrm.py
@@ -0,0 +1,19 @@
+"""
+pyXrm (c) 2023 Claas Huerkamp, All Rights Reserved
+"""
+
+
+def main() -> None:
+    """
+    Main entry point for pyXrm
+
+    Parameters:
+    None
+
+    Returns:
+    None
+    """
+
+
+if __name__ == "__main__":
+    main()
