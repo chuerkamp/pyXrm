158d5e7af771bcae5cd6a9adf74b586a0f154e75
Claas Hürkamp <claas.huerkamp@posteo.net>
2023-02-13 17:47:14 +0100

    Add application info und print message

--- src/application/application.py
+++ src/application/application.py
@@ -9,6 +9,16 @@ class Application:
     Main controller
     """
 
+    def __init__(self) -> None:
+        self.name = "pyXrm"
+        self.author_name: str = "Claas Huerkamp"
+        self.author_email: str = "claas.huerkamp@posteo.net"
+        self.version: str = "0.1.0"
+        self.license_message: str = "All rights Reserved"
+
+    def get_application_message(self) -> str:
+        return f"{self.name} Version: {self.version} (c) 2023 {self.author_name} <{self.author_email}>, {self.license_message}"
+
     def run(self) -> None:
         """
         Run main controller for pyXrm
